0b08b968deea85a8e5710e22fd6e171793b8afe8
Jonas Klimes <jonas.klimes@gmail.com>
2015-04-12 20:09:54 +0200

    add missing serialVersionUID

--- GUI/remote-gui/api/src/main/java/cz/dsllp/gui/api/message/command/Alert.java
+++ GUI/remote-gui/api/src/main/java/cz/dsllp/gui/api/message/command/Alert.java
@@ -5,6 +5,9 @@ package cz.dsllp.gui.api.message.command;
  * @since 09/04/15
  */
 public class Alert extends Command{
+
+    private static final long serialVersionUID = -1569324950703820178L;
+
     private String message;
 
     public Alert(String message) {
