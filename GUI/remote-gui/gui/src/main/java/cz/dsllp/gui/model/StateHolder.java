4ceee9bc2994134e0dd654bff560bdef52ce5794
Jonas Klimes <jonas.klimes@gmail.com>
2015-04-30 17:11:04 +0200

    create synchronized StateHolder

--- GUI/remote-gui/gui/src/main/java/cz/dsllp/gui/model/StateHolder.java
+++ GUI/remote-gui/gui/src/main/java/cz/dsllp/gui/model/StateHolder.java
@@ -11,4 +11,30 @@ import javax.inject.Singleton;
 public class StateHolder {
 
     private volatile GuiState state;
+    private volatile boolean onlyOneStep;
+    private volatile boolean stepProcessing;
+
+    public synchronized GuiState getState() {
+        return state;
+    }
+
+    public synchronized void setState(GuiState state) {
+        this.state = state;
+    }
+
+    public synchronized boolean isOnlyOneStep() {
+        return onlyOneStep;
+    }
+
+    public synchronized void setOnlyOneStep(boolean onlyOneStep) {
+        this.onlyOneStep = onlyOneStep;
+    }
+
+    public synchronized boolean isStepProcessing() {
+        return stepProcessing;
+    }
+
+    public synchronized void setStepProcessing(boolean stepProcessing) {
+        this.stepProcessing = stepProcessing;
+    }
 }
